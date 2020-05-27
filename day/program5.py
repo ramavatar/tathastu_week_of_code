e6723778e843a890583986a47ef7a8e3b4b8e4d5
ramavatar <65964480+ramavatar@users.noreply.github.com>
2020-05-27 20:56:43 +0530

    Create program5.py

---
+++ day/program5.py
@@ -0,0 +1,23 @@
+player1=int(input("Enter Score of player1: "))
+player2=int(input("Enter Score of player2: "))
+player3=int(input("Enter Score of player3: "))
+SR1=player1/.6
+SR2=player2/.6
+SR3=player3/.6
+print("StrikeRate of Player1:" , SR1)
+print("StrikeRate of Player2:" , SR2)
+print("StrikeRate of Player3:" , SR3)
+
+run1=player1*2
+run2=player2*2
+run3=player3*2
+print("Run of Player1 after Play 60 More Ball:" , run1)
+print("Run of Player2 after Play 60 More Ball:" , run2)
+print("Run of Player3 after Play 60 More Ball:" , run3)
+
+six1=int(player1/6)
+six2=int(player2/6)
+six3=int(player3/6)
+print("Max Six of Player1 :" , six1)
+print("Max Six of Player2 :" , six2)
+print("Max Six of Player3 :" , six3)
