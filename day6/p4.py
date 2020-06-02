4cfd67b649ca0960786096332406e34a569a605c
Ram Avatar Upadhyay <65964480+ramavatar@users.noreply.github.com>
2020-06-02 16:12:39 +0530

    Update p4.py

--- day6/p4.py
+++ day6/p4.py
@@ -1,17 +1,27 @@
-import math 
-def isPerfectSquare(x): 
-    s = int(math.sqrt(x)) 
-    return s*s == x 
-def isFibonacci(n): 
-    return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4) 
-     
-def ram(n):
-  for i in range(n): 
-     if (isFibonacci(i) == True): 
-         print ( i,"is a Fibonacci Number")
-     else: 
-         print (i,"is a not Fibonacci Number ")
-         
-num=int(input("ENTER THE RANGE: "))
-ram(num)
+def findNext(number,n): 
+     for i in range(n-1,0,-1): 
+         if number[i] > number[i-1]: 
+             break 
+     if i == 1 and number[i] <= number[i-1]: 
+         print ("Next number not possible")
+         return 
+     x = number[i-1] 
+     smallest = i 
+     for j in range(i+1,n): 
+         if number[j] > x and number[j] < number[smallest]: 
+             smallest = j 
+     number[smallest],number[i-1] = number[i-1], number[smallest] 
+     x = 0
+     for j in range(i):
+          x = x * 10 + number[j] 
+     number = sorted(number[i:]) 
+     for j in range(n-i): 
+         x = x * 10 + number[j] 
+       
+     print ("Next number with set of digits is",x)
+n = int(input("ENTER THE NUMBER Of DIGIT:  "))  
+num = int(input("ENTER "+  str(n)+" DIGIT NUMBER:  "))     
+res = [int(x) for x in str(num)]   
+print ("The list from number is " ,res)
+findNext(res,n)
 
